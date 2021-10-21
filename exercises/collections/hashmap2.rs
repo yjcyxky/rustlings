fe8126537f32fd65b58f92abcc9b8ebc3ba31c64
Jingcheng Yang <yjcyxky@users.noreply.github.com>
2021-10-21 08:41:52 +0000

    Finish collections chapter.

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -38,6 +37,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match basket.get(&fruit) {
+            None => {
+                basket.insert(fruit, 4);
+            },
+            _ => {},
+        }
     }
 }
 
