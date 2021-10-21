033c495cb09ad632aaf5c17c874889101d6c3755
Jingcheng Yang <yjcyxky@users.noreply.github.com>
2021-10-21 01:12:03 +0000

    Finish variables c chapter.

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,10 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+
+fn call_me() {
+    println!("This is the call_me function.");
+}
 
 fn main() {
     call_me();
